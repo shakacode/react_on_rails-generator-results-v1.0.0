8af8b21da42bb05f5e30e0038061fd71124ca62b
Rob Wise <robert.wise@outlook.com>
2015-11-02 20:46:10 -0500

    Basic-server-rendering App Generated v22

---
+++ tester_app/client/app/bundles/HelloWorld/startup/helloWorldAppServer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from '../containers/HelloWorld';
+
+const helloWorldAppServer.jsx = props => {
+  const reactComponent = (
+    <HelloWorld {...props} />
+  );
+  return reactComponent;
+};
+
+// Export is needed for the hot reload server
+export default helloWorldAppServer;
